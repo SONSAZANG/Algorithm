43bb04c74c491ed295623c64444e76664f9d9b31
sonsazang <62087798+SONSAZANG@users.noreply.github.com>
2024-12-26 11:41:34 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    
+    while(true)
+    {
+        if ( n < 10 ) 
+        {
+            answer += n;
+            break;
+        }
+        answer += n % 10;
+        n = n / 10;
+    }
+    
+    return answer;
+}
