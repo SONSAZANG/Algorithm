1df21a53727a30e192d4e0f0c47cc107cb10fcb3
sonsazang <62087798+SONSAZANG@users.noreply.github.com>
2024-12-28 14:56:06 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -6,18 +6,13 @@ using namespace std;
 
 bool solution(int x) {
     bool answer = true;
-    vector<int> vec;
     string s = to_string(x);
+    int sum = 0;
     for (int i = 0; i < s.size(); i++) {
         string a = s.substr(i, 1);
-        vec.push_back(stoi(a));
+        sum += stoi(a);
     }
     
-    int sum = 0;
-    for (int i : vec) {
-        cout << i << endl;
-        sum += i;
-    }
     answer = x % sum == 0;
     return answer;
 }
