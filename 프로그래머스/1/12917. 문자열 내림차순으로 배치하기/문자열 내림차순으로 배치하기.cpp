4593bae72e6e4f168e50ca1ec090a24135b5a1f6
sonsazang <62087798+SONSAZANG@users.noreply.github.com>
2024-12-31 11:41:41 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.cpp
@@ -1,29 +1,11 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
-bool compare(int a, int b) {
-    return a > b;
-}
-
 string solution(string s) {
-    string answer = "";
-    int size = s.length();
-    char cstr[size];
-    strcpy(cstr, s.c_str());
-    vector<int> intVec;
-    for (int i = 0; i < size; ++i) {
-        intVec.push_back(cstr[i] - 65);
-    }
-    sort(intVec.begin(), intVec.end(), compare);
-    
-    for (int i = 0; i < size; ++i) {
-        cstr[i] = intVec[i] + 65;
-    }
-    answer = cstr;
-    return answer;
+    sort(s.begin(), s.end(), greater<char>());    
+    return s;
 }
