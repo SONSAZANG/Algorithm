6d9ba14d80e29bff8f7495412de5fbd0d51615d2
sonsazang <62087798+SONSAZANG@users.noreply.github.com>
2025-01-06 10:15:07 +0900

    [level 1] Title: 시저 암호, Time: 0.06 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string s, int n) {
+    // 아스키코드 z -> a 25차이
+    int a = 0;
+    for(int i = 0; i < s.length(); i++) {
+        if (s[i] == ' ') {
+            continue;
+        }
+            
+        // 대문자 
+        if (s[i] <= 90) {
+            a = s[i] + n;
+            if (a > 90) {
+                a -= 26;
+            }
+            s[i] = a;
+        } 
+        // 소문자
+        else if (s[i] <= 122) {
+            a = s[i] + n;
+            if (a > 122) {
+                a -= 26;
+            }
+            s[i] = a;
+        }
+    }
+    
+    return s;
+}
