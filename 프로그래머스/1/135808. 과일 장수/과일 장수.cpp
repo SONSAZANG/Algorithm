4f8a859fca398d072a708f6d21ac6c8979d28d93
sonsazang <62087798+SONSAZANG@users.noreply.github.com>
2025-01-09 10:18:11 +0900

    [level 1] Title: 과일 장수, Time: 35.77 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -1,13 +1,12 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 using namespace std;
 
 
 int solution(int k, int m, vector<int> score) {
     int answer = 0;
-    vector<vector<int>> vc;
+    // 내림차순 정렬
     sort(score.begin(), score.end(), greater<int>());
     
     // 사용하지 않는 사과 제거
@@ -16,6 +15,8 @@ int solution(int k, int m, vector<int> score) {
         score.pop_back();
     }
     
+    // score를 순회하면서 m번째 자리의 숫자(m개로 배열을 나눴을때 최소값)를 조회
+    // score.size() = 12, m = 4 => 4, 8, 12를 찾는 방법
     for (int i = 0; i < score.size(); i++) {
         if ((i + 1) % m == 0) {
             answer += score[i] * m;
