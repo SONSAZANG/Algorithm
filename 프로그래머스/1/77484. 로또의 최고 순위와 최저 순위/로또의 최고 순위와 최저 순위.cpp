698e99fce866146109a211b22bae7aae7f14c1e7
sonsazang <62087798+SONSAZANG@users.noreply.github.com>
2025-01-15 09:45:51 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int zeroCnt = 0;
+int cnt = 0;
+vector<int> solution(vector<int> lottos, vector<int> win_nums) {
+    vector<int> answer;
+    int grade[10];
+    
+    grade[6] = 1;
+    grade[5] = 2;
+    grade[4] = 3;
+    grade[3] = 4;
+    grade[2] = 5;
+    grade[1] = 6;
+    grade[0] = 6;
+    
+    for(int i = 0; i < 6; i++) {
+        if(lottos[i] == 0) zeroCnt++;
+        auto it = find(win_nums.begin(), win_nums.end(), lottos[i]);
+        if (it != win_nums.end()) {
+            cnt++;
+            win_nums.erase(it);
+        }
+    }
+    
+    int gradeTop = grade[cnt + zeroCnt];
+    answer.push_back(gradeTop);
+    int gradeUnder = grade[cnt];
+    answer.push_back(gradeUnder);
+    
+    return answer;
+}
