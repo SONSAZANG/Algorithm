ea0fdea7376f5a543bf4997b8b09215e8e17b001
sonsazang <62087798+SONSAZANG@users.noreply.github.com>
2025-02-14 10:58:56 +0900

    [level 2] Title: 카펫, Time: 6.25 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,46 +1,35 @@
 #include <string>
 #include <vector>
-#include <iostream>
 using namespace std;
 
 vector<int> yellowFac;
-int x, y;
+int x, y, xCnt, yCnt;
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
     for (int i = 1; i <= yellow; i++) 
     {
-        if (yellow % i == 0) 
-        {
-            yellowFac.push_back(i);    
-        }
+        if (yellow % i == 0) yellowFac.push_back(i);    
     }
-    
     if (yellowFac.size() % 2 == 0) 
     {
-        int xCnt = yellowFac.size() / 2;
-        int yCnt = (yellowFac.size() / 2) - 1;
+        xCnt = yellowFac.size() / 2;
+        yCnt = (yellowFac.size() / 2) - 1;
         x = (yellowFac[xCnt]);
         y = (yellowFac[yCnt]);
-        
-        int b = (brown - 4) / 2;
-        
-        while (b != x + y) 
+        while ((brown - 4) / 2 != x + y) 
         {
             xCnt += 1;
             yCnt -= 1;
             x = (yellowFac[xCnt]);
             y = (yellowFac[yCnt]);
         }
-            
     }
     else 
     {
         x = (yellowFac[yellowFac.size() / 2]);
         y = (yellowFac[yellowFac.size() / 2]);
     }
-    
     answer.push_back(x + 2);
     answer.push_back(y + 2);
-    
     return answer;
 }
