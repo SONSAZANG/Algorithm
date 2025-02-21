c91367c75af17858ba588f82bb009f1cbd772317
sonsazang <62087798+SONSAZANG@users.noreply.github.com>
2025-02-21 10:54:44 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -3,21 +3,14 @@ using namespace std;
 
 int gcd(int a, int b)
 {
-    if (b == 0) 
-    {
-        return a;
-    }
+    if (b == 0) return a;
     else return gcd(b, a % b);
 }
 
 int solution(vector<int> arr) {
     int answer = 0;
-    
     sort(arr.begin(), arr.end(), greater<int>());
     answer = arr[0];
-    for (int i = 1; i < arr.size(); i++) 
-    {
-        answer = (answer * arr[i]) / gcd(answer, arr[i]);
-    }
+    for (int i = 1; i < arr.size(); i++) answer = (answer * arr[i]) / gcd(answer, arr[i]);
     return answer;
 } 
