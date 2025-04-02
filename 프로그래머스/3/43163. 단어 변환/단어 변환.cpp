53f69455fe5c0f7ae7c4cde67fad55c95254a7c5
sonsazang <62087798+SONSAZANG@users.noreply.github.com>
2025-04-02 13:49:09 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,9 +1,44 @@
-#include <string>
-#include <vector>
-
+#include<bits/stdc++.h>
 using namespace std;
 
+string s;
+int cnt;
 int solution(string begin, string target, vector<string> words) {
     int answer = 0;
+    
+    bool isExist = false;
+    for (string i : words) 
+    {
+        if (i == target) 
+        {
+            isExist = true;
+            break;
+        }
+    }
+    
+    if (!isExist) return 0;
+    
+    queue<pair<string, int>> q;
+    q.push({begin, 0});
+    while(q.size())
+    {
+        tie(s, cnt) = q.front(); q.pop();
+        for (int i = 0; i < words.size(); i++) 
+        {
+            int temp = 0;
+            for (int j = 0; j < s.length(); j++)
+            {
+                if (s[j] != words[i][j]) temp++;
+                if (temp > 1) break;
+            }
+            
+            if (temp == 1)
+            {
+                if (words[i] == target) return cnt + 1;
+                else q.push({words[i], cnt + 1});
+            }
+        }
+    }
+    
     return answer;
 }
